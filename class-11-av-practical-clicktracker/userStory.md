a0c351bec553e5878a407d55a264da5ff2a52c9e
James Branca <paceyachter@gmail.com>
2017-02-21 08:32:24 -0800

    busmall

---
+++ class-11-av-practical-clicktracker/userStory.md
@@ -0,0 +1,29 @@
+
+
+Marketing needs popularity metrics and analytics on the merchandise on sale.
+
+The marketing team would like access to a short description or survey from the participant about why they chose one option vs. another.
+
+The marketing team wants the questionnaire to be easy to fill out and for the data to be collected.
+
+The marketing would like to explore the possibility of having an alternate survey front-end design to see which prompts more engagement from the participants filling out the information.
+
+Marketing needs the user to have access to the average results once the questionnaire is completed.
+
+As a developer, I want to create a randomized questionnaire with a series of 15 choices of 3 pictures with no repeating images.
+
+As a developer, I need to write my code in an organized fashion by breaking down each line of code one by one- Also, i should make ample comments to enable me to easily fix bugs and implement new code.
+
+As a developer, I want my client to feel as though they need my design to increase efficiency and sales- this entails having quick access to quantifiable data which can be used for busmalls 'avg client'.
+
+As a developer, I would like a wow factor with my CSS and front-end development so that people won't skip the survey and will be motivated by design to take action.
+
+As a user, I want an appealing site to keep my motivated through the entire questionnaire.
+
+As a user, I want to have access to the results of others who took the questionnaire.
+
+As a user, I want the questionnaire to be quick and easy to use.
+
+As a user, I'd like the option to make purchases or find out more information on the company to check their reputation before doing business with them. (this might prompt the dev to focus on styling the survey to make the company appear as reputable as possible.)
+
+As a user, I'd like to tell the owner why specific products were more appealing and if I plan to shop with them based on the survey.
---
+++ img/class-11-av-practical-clicktracker/app.js
@@ -0,0 +1,165 @@
+'use strict';
+function Item(itemName, filePath){
+  this.itemName = itemName;
+  this.filePath = filePath;
+  this.clicks = 0;
+  this.clickCount = 0;
+  this.originalIndex = originalIndex++;
+};
+if (localStorage.getItem('totalClicks')){
+  var items = JSON.parse(localStorage.getItem('itemsArray'));
+  console.log('items array is');
+  console.log(items);
+} else {
+  var Image = [new Item('bag', '../assets/bag.jpg'),
+    new Image ('banana', '/assets/banana.jpg'),
+    new Image('boots', '../assets/boots.jpg'),
+    new Image('chair', '../assets/chair.jpg'),
+    new Image('cthulhu', '../assets/cthulhu.jpg'),
+    new Image('dragon', '../assets/dragon.jpg'),
+    new Image('pen', '../assets/pen.jpg'),
+    new Image('scissors', '../assets/scissors.jpg'),
+    new Image('shark', '/assets/shark.jpg'),
+    new Image('sweep', '../assets/sweep.png'),
+    new Image('unicorn','../assets/unicorn.jpg'),
+    new Image('usb', '../assets/usb.gif'),
+    new Image('water', '../assets/water-can.jpg'),
+    new Image('wine', '../assets/wine-glass.jpg'),];
+}
+
+var image = [];
+var img1 = document.getElementById('image1');
+var img2 = document.getElementById('image2');
+var img3 = document.getElementById('image3');
+
+ function displayImages(){
+    image[0] = Math.floor(Math.random() * items.length);
+  img1.src = items[image[0]].filePath;
+
+    image[1] = Math.floor(Math.random() * items.length);
+  while (image[1] === image[0])  {
+    image[1] = Math.floor(Math.random() * items.length);
+  }
+  img2.src = items[image[1]].filePath;
+
+  image[2] = Math.floor(Math.random() * items.length);
+  while (image[2] === image[0] || image[2] === image[1]) {
+    image[2] = Math.floor(Math.random() * items.length);
+  }
+  img3.src = items[image[2]].filePath;
+  console.log(image[0]);
+  console.log(image[1]);
+  console.log(image[2]);
+}
+displayImages();
+
+img1.setAttribute('src', items[image[0]].filePath);
+img2.setAttribute('src', items[image[1]].filePath);
+img3.setAttribute('src', items[image[2]].filePath);
+img1.addEventListener('click', handleClick);
+img2.addEventListener('click', handleClick);
+img3.addEventListener('click', handleClick);
+
+if (localStorage.getItem('totalClicks')){
+  var totalClicks = +localStorage.getItem('totalClicks');
+  console.log('totalClicks is ');
+  console.log(totalClicks);
+} else {
+  var totalClicks = 0;
+}
+
+function handleClick(event) {
+  if (event.path[0].id === 'image1'){
+    items[image[0]].clicks += 1;
+  }
+  else if(event.path[0].id === 'image2'){
+    items[image[1]].clicks += 1;
+  }
+  else{
+    items[image[2]].clicks += 1;
+  }
+  items[image[0]].clickCount += 1;
+  items[image[1]].clickCount += 1;
+  items[image[2]].clickCount += 1;
+  totalClicks += 1;
+  if (totalClicks === 3) {
+    resButton.removeAttribute('hidden');
+  }
+  displayImages();
+  makeClicksArray();
+  localStorage.setItem('itemsArray', JSON.stringify(items));
+  localStorage.setItem('totalClicks', totalClicks);
+}
+
+var clicksChartArray = [];
+function makeClicksArray(){
+  clicksChartArray = [];
+    for (var i = 0; i < items.length; i++){
+      clicksChartArray.push(items[i].clickCount)
+    }
+}
+
+var data = {
+    labels: [],
+    datasets: [
+        {
+            label: "Times Item is Shown",
+            fillColor: "#ff7070",
+            strokeColor: "#ff7070",
+            highlightFill: "#ff7070",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: []
+        },
+        {
+            label: "Times Item is Clicked",
+            fillColor: "#6b98e5",
+            strokeColor: "#6b98e5",
+            highlightFill: "#6b98e5",
+            highlightStroke: "rgba(151,187,205,1)",
+            data: []
+        }
+    ]
+};
+
+function newChartData(){
+  for (var d = 0; d < items.length; d++){
+    data.labels.push('0');
+    data.datasets[0].data.push('0');
+    data.datasets[1].data.push('0');
+  }
+}
+newChartData();
+var chartCanvas = document.getElementById('chartCanvas').getContext('2d');
+var chartMe = new Chart(chartCanvas).Bar(data);
+var clearsLS = document.getElementById('lsClear');
+
+
+var resButton = document.getElementById('result');
+var resButtonClicks = 0;
+resButton.addEventListener('click', resClick);
+
+var originalIndex = 0;
+
+function resClick(event) {
+  resButtonClicks += 1;
+  var results = document.getElementById('resultsBox');
+  results.removeAttribute('hidden');
+
+  items.sort(function (a, b) {return b.clicks - a.clicks;});
+    for(var i = 0; i < items.length; i++)
+    {
+      data.labels[i] = items[i].itemName;
+      chartMe.datasets[0].bars[i].value = items[i].clicks;
+      chartMe.datasets[1].bars[i].value = items[i].clickCount;
+    }
+    chartMe.update();
+    items.sort(function (a, b) {return a.originalIndex - b.originalIndex;});
+  }
+
+
+localStorage.setItem('num', 3);
+function clearLS(){
+  localStorage.clear();
+}
+clearButton = document.getElementById('clearLS');
+clearButton.addEventListener('click', clearLS);
